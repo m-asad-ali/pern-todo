8caf241de11365bbc3a03afb348253270c1a0710
m-asad-ali <masadali304@gmail.com>
2023-10-27 21:50:08 +0500

    before checking out to main

---
+++ frontend/src/redux/slices/authSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isAuthenticated: false,
+  token: null,
+  user: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.isAuthenticated = true;
+      state.token = action.payload.token;
+      state.user = action.payload.user;
+      console.log("user:", state.user);
+    },
+    logout: (state) => {
+      state.isAuthenticated = false;
+      state.token = null;
+      state.user = null;
+    },
+  },
+});
+export const { login, logout } = authSlice.actions;
+export default authSlice.reducer;
